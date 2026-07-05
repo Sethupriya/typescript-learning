ae4ea490a248352f1802f95edeef9220e231ffbf
Sethupriyadharshini <sethupriyadharshini@gmail.com>
2026-07-05 08:58:37 +0530

    Added Day3 Typescript examples

---
+++ Day3/TestJS.js
@@ -0,0 +1,24 @@
+/* "use strict";
+
+
+//TS is a Statically typed programming language.
+
+
+let data = 10; //number
+console.log(typeof (data)); //number
+data = "Ten";
+console.log(typeof (data)); //String
+console.log(data); //Ten */
+
+
+//TYPE SAFETY - "NOT"
+
+/* let message = "Hello"  //String
+let count = 30 //Number
+
+message = 100 //String
+
+console.log(message) //100 */
+
+/* let result = "5" + 3
+console.log(result) */
