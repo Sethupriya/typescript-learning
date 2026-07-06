82d716d56669662200ed2f6d49f537d526016640
Sethupriyadharshini <sethupriyadharshini@gmail.com>
2026-07-06 11:56:22 +0530

    Added Day 6

---
+++ Day6/DoWhileLoop.ts
@@ -0,0 +1,20 @@
+// do-while -> Statement execute and condition checks later
+
+//Example 1: print 1 to 5
+
+/* let i: number = 10
+do {
+    console.log(i)
+    i++
+} while (i <= 5) */
+
+
+//Example 2: 10,9,8,...1 descending order
+
+let i: number = 10
+do {
+    console.log(i)
+    i--
+} while (i > 0)
+
+export { }
---
+++ Day6/ForLoop.ts
@@ -0,0 +1,37 @@
+//For Loop -> when we know number of iterations before hand.
+
+// Example 1: print 1 to 10
+
+/* for (let i = 1; i <= 10; i++) {
+    console.log(i)
+} */
+
+/* console.log(`************* Example 2: Print even numbers ***************`)
+
+//Example 2: Print even numbers
+
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i)
+} */
+
+
+/* console.log(`************* Example 3: Print reverse numbers ***************`)
+
+//Example 3: Print reverse numbers
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i)
+} */
+
+
+//Example 4:
+
+/* let i: number //Global variable
+
+for (i = 1; i <= 5; i++) { //here i is local variable beacuse it is declared inside loop
+    console.log(i)
+}
+console.log(i) //undefined when i is in local variable and print 6 when i is global variable */
+/* for(i=1;i<=5;i++){ //here i is global variable beacuse it is not re declared inside loop
+
+} */
