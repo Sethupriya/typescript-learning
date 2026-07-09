bb0ad6f63a77d4ea97c55ac5db8ad5359ed01f3e
Sethupriyadharshini <sethupriyadharshini@gmail.com>
2026-07-09 12:51:26 +0530

    Changed variable value in main branch

--- Day4/TernaryOperator.ts
+++ Day4/TernaryOperator.ts
@@ -8,7 +8,7 @@ console.log(res)//100
 
 
 //Example 2:
-let personAge: number = 30;
+let personAge: number = 20;
 
 let res1: string = (personAge >= 18) ? "Adult" : "Minor"
 console.log(res1)//Adult
